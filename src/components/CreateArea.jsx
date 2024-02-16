7a417194fb71c6c4793a9affe05fb738c85d186d
Tajul Islam Sumon <tajul.islam@northsouth.edu>
2024-02-16 20:51:36 +0600

    Keeper App Final Version

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    const [Note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function addNewNote(event){
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name] : value
+            }
+        });
+    }
+
+    function submitNote(event){
+        props.onAdd();
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+        <form>
+            <input onChange={addNewNote} name="title" placeholder="Title" value={Note.title}/>
+            <textarea onChange={addNewNote} name="content" placeholder="Take a note..." rows="3" value={Note.content}/>
+            <button onClick={submitNote}>Add</button>
+        </form>
+        </div>
+  );
+}
+
+export default CreateArea;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Note(props){
-    return (
-        <div className="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
-        </div>
-    );
-};
+function Note(props) {
+  function afterClick(){
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={afterClick}>DELETE</button>
+    </div>
+  );
+}
 
 export default Note;
